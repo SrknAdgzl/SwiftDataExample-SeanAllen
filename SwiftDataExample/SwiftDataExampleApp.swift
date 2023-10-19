0192007c184fa02c7036da166f685b9bdf5d0d73
SrknAdgzl <138037543+SrknAdgzl@users.noreply.github.com>
2023-10-19 16:38:29 +0300

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Serkan ADIGÜZEL on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
