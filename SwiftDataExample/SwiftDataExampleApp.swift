9c2368e962a246d517f1dc32f7e948a82f245439
SrknAdgzl <138037543+SrknAdgzl@users.noreply.github.com>
2023-10-19 18:00:30 +0300

    The app is finished. This app uses SwiftData to deal with the data. Sean Allen's YouTube  video  for intro to SwiftData.

--- SwiftDataExample/SwiftDataExampleApp.swift
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataExampleApp: App {
+    // (1) this is where we create our container. by building here all our app can use it
+    let container: ModelContainer = {
+        let schema = Schema([Expense.self])
+        let container = try! ModelContainer(for: schema, configurations: [])
+        return container
+    }()
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(container)
     }
 }
